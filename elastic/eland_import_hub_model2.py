30e408571d29c008d051eb504b2fc6eef6482d67
EyadSwaity <iyad.alswaiti@external.stellantis.com>
2023-05-23 11:18:24 +0300

    fixed elastic.

--- elastic/eland_import_hub_model2.py
+++ elastic/eland_import_hub_model2.py
@@ -48,16 +48,16 @@ def get_arg_parser():
     parser = argparse.ArgumentParser("elastic")
     location_args = parser.add_mutually_exclusive_group(required=True)
 
-    # location_args.add_argument(
-    #     "--url",
-    #     default="https://49a88e589a0a4bad9350451ebeae8797.eastus2.azure.elastic-cloud.com",
-    #     help="An Elasticsearch connection URL, e.g. http://localhost:9200",
-    # )
     location_args.add_argument(
-        "--cloud-id",
-        default="Elastic-05559-s-001:ZWFzdHVzMi5henVyZS5lbGFzdGljLWNsb3VkLmNvbTo0NDMkNDlhODhlNTg5YTBhNGJhZDkzNTA0NTFlYmVhZTg3OTckMDdlYjU1NzhjODdlNGI3MWI5NmIwNjY0ZmY3NWI4ODc=",
-        help="Cloud ID as found in the 'Manage Deployment' page of an Elastic Cloud deployment",
+        "--url",
+        default="https://49a88e589a0a4bad9350451ebeae8797.eastus2.azure.elastic-cloud.com",
+        help="An Elasticsearch connection URL, e.g. http://localhost:9200",
     )
+    # location_args.add_argument(
+    #     "--cloud-id",
+    #     default="Elastic-05559-s-001:ZWFzdHVzMi5henVyZS5lbGFzdGljLWNsb3VkLmNvbTo0NDMkNDlhODhlNTg5YTBhNGJhZDkzNTA0NTFlYmVhZTg3OTckMDdlYjU1NzhjODdlNGI3MWI5NmIwNjY0ZmY3NWI4ODc=",
+    #     help="Cloud ID as found in the 'Manage Deployment' page of an Elastic Cloud deployment",
+    # )
     parser.add_argument(
         "--hub-model-id",
         default="bart-large-mnli",
